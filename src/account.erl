ced4ca5e1a9dd065b7b07be3742324688f2f240e
Bastiaan Rasing <bastiaan@rasing.me>
2014-01-31 12:18:56 +0100

    store new accounts in Postgresql.

--- src/account.erl
+++ src/account.erl
@@ -2,13 +2,25 @@
 
 -export([create/2]).
 
-create(_Username, Password) ->
-  %  {ok, _C} = pgsql:connect("127.0.0.1", "postgres", "postgres", [{database, "test"}, {port, 6432}]),
+create(Username, Password) ->
   %  {ok, _Columns, _Rows} = pgsql:squery(_C, ["SELECT * FROM users WHERE user_id = ", "1"]),
   %  io:format("~p~n", [_Rows]),
   {ok, Salt} = bcrypt:gen_salt(),
   {ok, Hash} = bcrypt:hashpw(Password, Salt),
-  io:format("~p~n", [Hash]),
+  GUID = ossp_uuid:make(v4, text),
 
-  {ok, ossp_uuid:make(v4, text)}.
+  {ok, DbAddr} = application:get_env(mijngewicht_server, db_addr),
+  {ok, DbUser} = application:get_env(mijngewicht_server, db_username),
+  {ok, DbPass} = application:get_env(mijngewicht_server, db_password),
+  {ok, DB} = application:get_env(mijngewicht_server, db),
+  {ok, DbPort} = application:get_env(mijngewicht_server, db_port),
+  {ok, C} = pgsql:connect(DbAddr, DbUser, DbPass, [{database, DB}, {port, DbPort}]),
+  _Blah = pgsql:squery(C, [
+      "INSERT INTO accounts (account_guid, username, password) VALUES ('",
+      GUID, "', '",
+      Username, "', '",
+      Hash, "')"
+    ]),
+  {ok, GUID}.
+
 
